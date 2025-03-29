1364324ef837eee4c1f4f55700727aaf2f6eb033
Alexey Maxev <maxev.alexei@yandex.ru>
2025-03-29 11:50:25 +0300

    мелкие изменения

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
-//==============================================================================
-int main() {
+#include <libopencm3/stm32/rcc.h>   // reset and clock control
+#include <libopencm3/stm32/gpio.h>  // general purpose input-output
 
-    while (true) {
+class Rcc{
+public:
+    Rcc(){
+        rcc_clock_setup_pll(&rcc_hse_16mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+    };
+};
+
+Rcc clock_system;
+
+
+int main() 
+{
+    rcc_periph_clock_enable(RCC_GPIOD);
+
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15 | GPIO13);
+
+
+    for (uint32_t ptime{0}; true; ++ptime) //Запись интерации в число ptime
+    {
+        gpio_toggle(GPIOD, GPIO13);
+        if ( ptime % 500'000 < 250'000)
+        gpio_set(GPIOD, GPIO15);
+        else
+        gpio_clear(GPIOD, GPIO15);
 
     }
+    
+
+
+/*
+    while (true) 
+    {
+        gpio_toggle(GPIOD, GPIO15); //Переключение сигнала
+        for (volatile uint32_t i=0; i < 500'000; ++i);
+
+    }
+*/
+
 }
